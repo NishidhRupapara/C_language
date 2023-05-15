8bd81cda2c2c08380cf860938d3dc364ab528ae4
NishidhRupapara <nishidhnishidh8@gmail.com>
2023-05-15 17:36:48 +0530

    ch

---
+++ ch-13/c13-1.c
@@ -0,0 +1,29 @@
+/*1.WAP to read content from one file & write it to
+another file.*/
+
+// 2=>1
+
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+	FILE *p1;
+	FILE *p2;
+	
+	
+	
+	char a[30];
+	
+	p1 =fopen("C:\\Users\\r22\\Desktop\\ch13-1.txt.txt","w");
+		
+	p2 =fopen("C:\\Users\\r22\\Desktop\\ch13-2.txt.txt","r");
+	
+	fscanf(p2,"%[^\n]",&a);
+	
+	printf("%s",a);
+	fprintf(p1,"%s",a);
+	
+	
+	
+}
